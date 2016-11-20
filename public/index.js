cd2c7f436d0bfe717ec4e2e2e9b11afe8ba028da
Shivang Saxena <shivangs44@gmail.com>
2016-11-20 09:53:31 -0500

    👣💒 Updated with HyperDev

--- public/index.js
+++ public/index.js
@@ -1,14 +1,29 @@
 
-$('.search').click(function(e){
+$('#mainForm').submit(function(e){
   e.preventDefault();
-  window.location="/view";
+  
+  // Get the type the entered
+  var typeValue = $('.typeValue').val();
+
+ 
+  // get the zipcode they entered
+  var zipCode=$('.zipCode').val();
+  
+  
+  // build url like this: /view?type=[TYPE]&zipcode=[ZIPCODE]
+  var url = "/view?type=" + typeValue + "&zipcode=" + zipCode;
+
+
+  // redirect to that url  
+  window.location = url;
+  
 });
 
-$('.zip').keypress(function(e){
-  if (e.which == 13) {
-      window.location="/view";
-  }
-});
+// // $('.zip').keypress(function(e){
+// //   if (e.which == 13) {
+// //       window.location="/view";
+// //   }
+// });
 
 
 
--- public/view.js
+++
@@ -1,5 +0,0 @@
-$('.Location').css('color','red');
-$('.but').click(function(){
-  window.location= "/public/index.html"
-});
-
